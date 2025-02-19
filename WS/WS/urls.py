13d5029b2f0f1d08b705fd9d3b0cb2840ecb5883
Piyush <piyushoberoy14@gmail.com>
2025-02-19 09:49:57 +0530

    Adding PGSQL

--- WS/WS/urls.py
+++ WS/WS/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name='home'),
@@ -26,3 +27,5 @@ urlpatterns = [
     path('dictionary/', include('dictApp.urls')),
     path('user/', include('userApp.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
